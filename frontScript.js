57a73999e4502fe454881f6859e0312687fe9718
mcsandovall <rasec_oiram@live.com.mx>
2022-03-21 23:35:15 -0500

    text display page implemented

---
+++ frontScript.js
@@ -0,0 +1,22 @@
+
+// functions for the drop down menu
+function dropFunction(){
+    document.getElementById("dropdown").classList.toggle("show")
+}
+
+// filter function
+function filterFunction() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementById("dropSearch");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("dropdown");
+    a = div.getElementsByTagName("a");
+    for (i = 0; i < a.length; i++) {
+        txtValue = a[i].textContent || a[i].innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        a[i].style.display = "";
+        } else {
+        a[i].style.display = "none";
+        }
+    }
+}
