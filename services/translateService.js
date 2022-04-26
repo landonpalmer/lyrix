74f783da7fd256f3d69544f1532bee7be5c093b2
mcsandovall <rasec_oiram@live.com.mx>
2022-04-26 09:50:30 -0500

    new api key added and all the functions tested

--- services/translateService.js
+++ services/translateService.js
@@ -5,11 +5,11 @@
  * @Note There is a 500 request limit between this API and the lanague detector, and a 5r/s cap
  */
 
-const getTranslation = async (text,Oacro,Tacro) => {
+const getTranslation = async (text,Tlang,Olang) => {
     const encodedParams = new URLSearchParams();
     encodedParams.append("q", text);
-    encodedParams.append("target", Tacro);
-    encodedParams.append("source", Oacro);
+    encodedParams.append("target", Tlang);
+    encodedParams.append("source", Olang);
 
     const options = {
         method: 'POST',
@@ -17,13 +17,18 @@ const getTranslation = async (text,Oacro,Tacro) => {
             'content-type': 'application/x-www-form-urlencoded',
             'Accept-Encoding': 'application/gzip',
             'X-RapidAPI-Host': 'google-translate1.p.rapidapi.com',
-            'X-RapidAPI-Key': 'd616f8c901msh20e2bf991e7104dp11d7f8jsndbc55af464ae'
+            'X-RapidAPI-Key': 'd4db23b5e3msh43209409a0520f9p148ebdjsnc2636ceb0fbf'
         },
         body: encodedParams
     };
 
     fetch('https://google-translate1.p.rapidapi.com/language/translate/v2', options)
-        .then(response => response.json())
-        .then(response => console.log(response))
+        .then(response => response.text())
+        .then((response) => {
+            var data = JSON.parse(response);
+            var translateText = data.data.translations[0].translatedText;
+            console.log(translateText);
+            return translateText;
+        })
         .catch(err => console.error(err));
 }
